8da91837802587b18a531b53b0f8918001d6d0b0
ctnckr <cetinceker@arkingroup.com>
2024-10-25 19:25:42 +0300

    2.9_Diziler (Numeric Arrays)

---
+++ examples/diziler.php
@@ -0,0 +1,23 @@
+<?php
+    //Diziler (Array)
+
+    //Numeric Arrays
+    $renk1 = "Kırmızı";
+    $renk2 = "Mavi";
+    $renk3 = "Sarı";
+
+    $renkler = array("Kırmızı","Mavi","Sarı");
+
+    echo gettype($renkler)."<br>";
+    echo $renkler[0]."<br>";
+    echo $renkler[1]."<br>";
+    echo $renkler[2]."<br>";
+
+    $sayilar = array(13,5435,985685,23424,25);
+
+    echo $sayilar[0]."<br>";
+    echo $sayilar[1]."<br>";
+    echo $sayilar[4]."<br>";
+
+
+?>
