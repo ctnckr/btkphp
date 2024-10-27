0649c4a5cbacd35e41c643f28494a5c6e17c37fe
ctnckr <cetinceker@arkingroup.com>
2024-10-27 22:49:46 +0200

    2.10_Associative Arrays: son commit

--- examples/associative-arrays.php
+++ examples/associative-arrays.php
@@ -1,6 +1,33 @@
 <?php 
-// Associative Arrays (Key-Value):  İlişkisel diziler
-// key-value => 34: istanbul, 53: Rize
+    // Associative Arrays (Key-Value):  İlişkisel diziler
+    // key-value => 34: istanbul, 53: Rize
 
+    $plakalar = array("41","53","34");
+    $sehirler = array("kacaeli","rize","istanbul");
+
+    echo "$plakalar[0] : $sehirler[0]"."<br>";
+    echo "$plakalar[1] : $sehirler[1]"."<br>";
+    echo "$plakalar[2] : $sehirler[2]"."<br>";
+
+    //key-value mantığıyla dizi
+    $plaka_bilgileri = array (
+        "41" => "kocaeli",
+        "53" => "rize",
+        "34" => "istanbul"
+    );
+
+    echo $plaka_bilgileri["41"]."<br>";
+    echo $plaka_bilgileri["53"]."<br>";
+    echo $plaka_bilgileri["34"]."<br>";
+
+
+    $telefon_rehberi = array (
+        "ali" => "5555555",
+        "mehmet" => "444444",
+        "ayşe" => "33333"
+    );
+
+    echo $telefon_rehberi["ali"];
+    
 
 ?>
