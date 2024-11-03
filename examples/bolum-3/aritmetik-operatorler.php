48705c635dcfc0a1ac74b4eb6ce5451836699bf2
ctnckr <cetinceker@arkingroup.com>
2024-11-03 17:39:59 +0200

    3.1_Aritmetik Operatörler

---
+++ examples/bolum-3/aritmetik-operatorler.php
@@ -0,0 +1,42 @@
+<?php
+
+    $a = 2;
+    $b = 5;
+
+    // echo $a + $b; //toplama
+    // echo "<br>";
+
+    // echo $a - $b; //çıkarma
+    // echo "<br>";
+
+    // echo $a * $b; //çarpma
+    // echo "<br>";
+
+    // echo $a / $b; //bölme
+    // echo "<br>";
+
+    // echo $a % $b; //mod alma.
+    // echo "<br>";
+
+    // echo $a ** $b; //üs alma
+    // echo "<br>";
+
+    ///////////////////////
+
+    //echo $a++; //işlem sonrası 1 arttır
+    //echo "<br>";
+    //echo $a;
+
+    // echo ++$a; //işlem öncesi 1 arttır
+    // echo "<br>";
+    // echo $a;
+
+    // echo $a--; //işlem sonrası 1 azalt
+    // echo "<br>";
+    // echo $a;
+
+    echo --$a; //işlem öncesi 1 azalt
+    echo "<br>";
+    echo $a;
+
+?>
