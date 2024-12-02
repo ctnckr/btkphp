543e0319272769aae2f7b1ff94fb5a4f783e0a86
ctnckr <cetinceker@arkingroup.com>
2024-12-02 13:58:24 +0200

    4.2_if-else Bloğu

---
+++ examples/bolum-4/ifelse.php
@@ -0,0 +1,22 @@
+<?php
+
+    //Koşullu Durumlar 
+        //1- if-else
+        //2- Switch Bloğu
+        //3- Ternary Operatörü
+
+    $username = "admin";
+    $password = "1234";
+
+    if ($username == "admin") {
+        
+        if ($password == "1234"){
+            echo "Merhaba BTK";
+        } else {
+            echo "parola yanlış";
+        }
+    } else {
+        echo "username yanlış";
+    }
+
+?>
